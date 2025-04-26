9e022628e9aa9135a847be8a21946d3edc90fdef
Vaibhavi <vaibhavichaudhari304@gmail.com>
2025-04-27 01:31:06 +0530

    updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,9 +14,6 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "My Portfolio",
   description: "Welcome to My Portfolio",
-  icons:{
-    icon: "/favicon.ico",
-  }
 };
 
 export default function RootLayout({ children }) {
