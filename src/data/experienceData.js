30ba8e985944f0d423e38b639b3d8345d69d5235
Vaibhavi <vaibhavichaudhari304@gmail.com>
2025-04-27 00:26:28 +0530

    framer motion

---
+++ src/data/experienceData.js
@@ -0,0 +1,35 @@
+export const experiences = [
+    {
+  
+      company: "Business Web-Solution",
+      role: "Full Stack Developer",
+      skills: ["React", "Node.js", "MongoDB", "Tailwind"],
+      location: "New Delhi",
+      type: "Remote",
+      period: "May 2024 - Aug 2024"
+    },
+    {
+      company: "Toshal Infotech Pvt. Ltd.",
+      role: "Web Designer",
+      skills: ["HTML5", "JavaScript", "CSS", "Bootstrap", "SASS"],
+      location: "Surat",
+      type: "On-site",
+      period: "Jan 2024 - Apr 2024"
+    },
+    {
+      company: "Edunet Foundation",
+      role: "Internship",
+      skills: ["Internet of Things (IOT)", "Kali Linux", "SAP Analytics Cloud", "Cybersecurity", "Sensors", "Data Visualisation", "Microsoft Power BI"],
+      location: "Bardoli",
+      type: "Onsite",
+      period: "Jul 2023 - Aug 2023"
+    },
+    {
+      company: "Sparks To Ideas",
+      role: "Python Intern",
+      skills: ["Python"],
+      location: "Ahmedabad",
+      type: "Remote",
+      period: "Jun 2023 - Jun 2023"
+    }
+  ];
